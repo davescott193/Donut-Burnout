64ac75d973adfd32d604775fbb1e9d7761caff35
Allister Hamilton <allister.ham@gmail.com>
2022-05-25 14:40:59 +1200

    creating main menu and basic foundation for scripts

---
+++ Donut Burnout/Assets/Scripts/ButtonMechanics.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+/*
+ 
+ Bachelor of Software Engineering
+ Media Design School
+ Auckland
+ New Zealand
+
+ (c) 2021 Media Design School
+
+ File Name    : ButtonMechanics.cs
+ Description  : mechanics for what buttons do
+ Author       : Allister Hamilton
+
+*/
+
+
+public class ButtonMechanics : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [Header("Toggle below to activate button function. Hover over anything to read info. Ususally a button will only have one toggle ticked.")]
+
+    [Header("Start Menu Buttons")]
+
+    [Tooltip("Opens the level Menu")]
+    public bool OpenLevelBool;
+    public bool CloseLevelBool;
+    [Tooltip("Opens the controls Menu")]
+    public bool OpenControlsBool;
+
+    [Tooltip("Opens the settings Menu")]
+    public bool OpenSettingsBool;
+
+
+    [Tooltip("Fully closes the application. It won't work in Unity Editor, only when built.")]
+    public bool ExitGameBool;
+
+    [Header("Controls Menu Buttons")]
+
+    [Tooltip("Close the Controls Menu")]
+    public bool CloseControlsBool;
+
+    [Header("Settings Menu Buttons")]
+
+    [Tooltip("Close the settings Menu")]
+    public bool CloseSettingsBool;
+
+
+    [Tooltip("Toggles the shadows on and off")]
+    public bool ShadowsToggleBool;
+
+    [Tooltip("Turns off rotating cameras etc to save on fps")]
+    public bool PerformanceToggleBool;
+
+    [Tooltip("Sound or Music name")]
+    public string PoolName;
+
+    [Tooltip("Speed the camera rotates")]
+    public bool CameraSpeedBool;
+
+    void OnEnable()
+    {
+        if (ShadowsToggleBool)
+        {
+            if (PlayerPrefs.GetInt("Shadows") == 1)
+            {
+                QualitySettings.shadows = ShadowQuality.Disable;
+                transform.GetChild(1).GetComponent<Text>().text = "Shadows: ON";
+            }
+            else
+            {
+                QualitySettings.shadows = ShadowQuality.All;
+                transform.GetChild(1).GetComponent<Text>().text = "Shadows: OFF";
+            }
+        }
+
+        if (PerformanceToggleBool)
+        {
+            if (PlayerPrefs.GetInt("Performance") == 1)
+            {
+                transform.GetChild(1).GetComponent<Text>().text = "Performance: ON";
+            }
+            else
+            {
+                transform.GetChild(1).GetComponent<Text>().text = "Performance: OFF";
+            }
+        }
+
+        if (PoolName != "")
+        {
+            transform.GetChild(0).GetComponent<Slider>().value = PlayerPrefs.GetFloat(PoolName);
+        }
+
+        if (CameraSpeedBool)
+        {
+            transform.GetChild(0).GetComponent<Slider>().value = PlayerPrefs.GetFloat("Camera Speed");
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        float Scale = 1f;
+        transform.localScale = new Vector3(Scale, Scale, Scale);
+
+        if (GetComponent<Animation>())
+            GameManager.AnimationChangeDirection(GetComponent<Animation>(), "Select", false).wrapMode = WrapMode.Once;
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        float Scale = 1.02f;
+        transform.localScale = new Vector3(Scale, Scale, Scale);
+
+        if (GetComponent<Animation>())
+            GameManager.AnimationChangeDirection(GetComponent<Animation>(), "Select").wrapMode = WrapMode.PingPong;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        float Scale = .98f;
+        transform.localScale = new Vector3(Scale, Scale, Scale);
+        GameManager.instance.SoundPool.PlaySound(GameManager.instance.ButtonDownSound);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        GameManager.instance.SoundPool.PlaySound(GameManager.instance.ButtonUpSound);
+        OnPointerEnter(eventData);
+        DoFunction();
+    }
+
+    public void DoFunction()
+    {
+
+        if (OpenLevelBool)
+        {
+            GameManager.AnimationChangeDirection(MainMenu.instance.GetComponent<Animation>(), "Level Menu Change");
+        }
+
+        if (CloseLevelBool)
+        {
+            GameManager.AnimationChangeDirection(MainMenu.instance.GetComponent<Animation>(), "Level Menu Change", false);
+        }
+
+
+        if (PoolName != "")
+        {
+            PlayerPrefs.SetFloat(PoolName, transform.GetChild(0).GetComponent<Slider>().value);
+
+            GameManager.instance.MusicPool.RefreshAudioVolumes();
+            GameManager.instance.SoundPool.RefreshAudioVolumes();
+        }
+
+        if (CameraSpeedBool)
+        {
+            PlayerPrefs.SetFloat("Camera Speed", transform.GetChild(0).GetComponent<Slider>().value);
+        }
+
+        if (OpenSettingsBool)
+        {
+            GameManager.AnimationChangeDirection(MainMenu.instance.GetComponent<Animation>(), "Settings Menu Change");
+        }
+
+        if (CloseSettingsBool)
+        {
+            GameManager.AnimationChangeDirection(MainMenu.instance.GetComponent<Animation>(), "Settings Menu Change", false);
+        }
+
+        if (OpenControlsBool)
+        {
+            GameManager.AnimationChangeDirection(MainMenu.instance.GetComponent<Animation>(), "Controls Menu Change");
+        }
+
+        if (CloseControlsBool)
+        {
+            GameManager.AnimationChangeDirection(MainMenu.instance.GetComponent<Animation>(), "Controls Menu Change", false);
+        }
+        if (ExitGameBool)
+        {
+            Application.Quit();
+        }
+
+        if (PerformanceToggleBool)
+        {
+            PlayerPrefs.SetInt("Performance", PlayerPrefs.GetInt("Performance") == 1 ? 0 : 1);
+            OnEnable();
+        }
+
+        if (ShadowsToggleBool)
+        {
+            PlayerPrefs.SetInt("Shadows", PlayerPrefs.GetInt("Shadows") == 1 ? 0 : 1);
+            OnEnable();
+        }
+    }
+}
---
+++ Donut Burnout/Assets/Scripts/GameManager.cs
@@ -0,0 +1,291 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//using UnityEngine.Rendering.PostProcessing;
+
+
+/*
+ 
+ Bachelor of Software Engineering
+ Media Design School
+ Auckland
+ New Zealand
+
+ (c) 2021 Media Design School
+
+ File Name    : GameManager.cs
+ Description  : holds vitial functions and loading scenes
+ Author       : Allister Hamilton
+
+*/
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public AudioPool MusicPool;
+    public AudioPool SoundPool;
+
+    public Animation SceneAnimation;
+    Coroutine LoadingCoroutine;
+    // public PostProcessResources postProcessResources;
+
+    [Header("Audio Clip Referencing")]
+    public AudioClip ButtonDownSound;
+    public AudioClip ButtonUpSound;
+    [Space(10)]
+    public AudioClip LoadingSound;
+
+    [System.Serializable]
+    public class ToggleData
+    {
+        public string ToggleName;
+        public Color ToggleColor;
+    }
+
+    private void Awake()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            if (PlayerPrefs.GetInt("First Time") == 0)
+            {
+                PlayerPrefs.SetInt("First Time", 1);
+
+                PlayerPrefs.SetFloat("Music", .4f);
+                PlayerPrefs.SetFloat("Sound", 1);
+                PlayerPrefs.SetFloat("Camera Speed", 2);
+
+                PlayerPrefs.SetInt("Highest Level", 1);
+
+                PlayerPrefs.SetInt("Performance", 1);
+                PlayerPrefs.SetInt("Shadows", 1);
+            }
+
+
+        }
+        firstBool = true;
+        ChangeScene(0);
+    }
+    bool firstBool;
+    public void ChangeScene(int sceneNumber = -1)
+    {
+        if (sceneNumber == -1)        //doing sceneindex as -1 will assume its just restarting scene
+            sceneNumber = SceneManager.GetActiveScene().buildIndex;
+
+
+        PauseChange(true);
+
+        if (LoadingCoroutine != null)
+        {
+            StopCoroutine(LoadingCoroutine);
+        }
+
+        LoadingCoroutine = StartCoroutine(ChangeSceneIEnumerator(sceneNumber));
+    }
+
+    public static void PauseChange(bool Show)
+    {
+        Time.timeScale = Show ? 1 : 0;
+    }
+
+    public static void CursorChange(bool Show)
+    {
+        Cursor.visible = Show;
+        Cursor.lockState = Show ? CursorLockMode.None : CursorLockMode.Locked;
+    }
+
+    IEnumerator ChangeSceneIEnumerator(int sceneNumber)
+    {
+        AnimationState animationState = AnimationChangeDirection(GetComponent<Animation>(), "Load", true, firstBool);
+
+        GameManager.instance.MusicPool.DynamicVolumeChange(-1);
+
+        yield return new WaitUntil(() => !animationState.enabled);
+
+        GameManager.instance.SoundPool.PlaySound(GameManager.instance.LoadingSound);
+
+        if (!firstBool)
+        {
+            AsyncOperation operation = SceneManager.LoadSceneAsync(sceneNumber);
+
+            yield return new WaitUntil(() => operation.isDone);
+        }
+        firstBool = false;
+        animationState = AnimationChangeDirection(GetComponent<Animation>(), "Load", false);
+        animationState.time = 0.5f;
+        GameManager.CursorChange(true);
+        LoadingCoroutine = null;
+
+        // GameManager.instance.MusicPool.PlayMusic(SceneMusicList[sceneNumber]);
+    }
+
+
+    public static float ReturnThresholds(float Size, int MaxSize, int MinSize = 0, bool WrapAround = true)
+    {
+        //Debug.Log(MinSize + " < " + Size + " < " + MaxSize);
+
+        for (int i = 0; i < 1; i++)
+        {
+            if (Size < MinSize)
+            {
+                //-1 = 4 + (0 - - 1)
+                if (WrapAround)
+                {
+                    Size = MaxSize + (MinSize + Size) + 1;
+                    i = -1;
+                }
+                else
+                {
+                    Size = MinSize;
+                }
+
+                continue;
+            }
+
+            if (Size > MaxSize)
+            {
+                if (WrapAround)
+                {
+                    Size = MinSize + (Size - MaxSize) - 1;
+                    i = -1;
+                }
+                else
+                {
+                    Size = MaxSize;
+                }
+
+                continue;
+            }
+        }
+
+        return Size;
+    }
+
+
+    public static int ReturnBitShift(string[] LayerNames = null, bool Exclude = false)
+    {
+        if (LayerNames == null)
+        {
+            LayerNames = new string[] { "Default" };
+        }
+
+        int layerMask = 0;
+
+        for (int i = 0; i < LayerNames.Length; i++)
+        {
+            layerMask = layerMask | 1 << LayerMask.NameToLayer(LayerNames[i]);
+        }
+
+        if (Exclude)
+        {
+            layerMask = ~layerMask;
+        }
+
+        return layerMask;
+    }
+
+    public static void ResetChosenHolder(Transform Holder, int StartNumber = 0)
+    {
+        List<Transform> AllChilds = new List<Transform>();
+
+        for (int i = StartNumber; i < Holder.childCount; i++)
+        {
+            AllChilds.Add(Holder.GetChild(i));
+        }
+
+        for (int i = 0; i < AllChilds.Count; i++)
+        {
+            Destroy(AllChilds[i].gameObject);
+            AllChilds[i].gameObject.SetActive(false);
+            AllChilds[i].SetParent(null);
+        }
+    }
+
+    public static void ChangeAnimationLayers(Animation ChosenAnimation)
+    {
+        if (ChosenAnimation)
+        {
+            int Index = 0;
+            foreach (AnimationState clip in ChosenAnimation)
+            {
+                ChosenAnimation[clip.name].layer = Index;
+                ChosenAnimation[clip.name].speed = 1;
+                Index += 1;
+            }
+        }
+    }
+
+    public static AnimationState AnimationChangeDirection(Animation ChosenAnimation, string AnimationName = "", bool Forward = true, bool Instant = false)
+    {
+        if (ChosenAnimation)
+        {
+            //Debug.Log(ChosenAnimation.name + " " + AnimationName);
+            //AnimationName = "" will default to the first animation
+
+            if (ChosenAnimation.GetClip(AnimationName) || ChosenAnimation.clip)
+            {
+
+                if (!ChosenAnimation.GetClip(AnimationName) && ChosenAnimation.clip)
+                {
+                    if (AnimationName != "")
+                    {
+                        Debug.LogError("Animation Missing for " + AnimationName + " so will be defaulting to first clip");
+                    }
+
+                    AnimationName = ChosenAnimation.clip.name;
+                }
+
+
+                //Time Types:
+
+                //0 = Will continue from where it was interupted
+                //1 = Will start from fresh
+                //2 = Will instantly reach one side of the animation
+
+
+                if (Forward)
+                {
+
+                    if (Instant)
+                    {
+                        ChosenAnimation[AnimationName].time = ChosenAnimation[AnimationName].length;
+                    }
+
+                    ChosenAnimation[AnimationName].speed = 1;
+                }
+                else
+                {
+                    if (ChosenAnimation[AnimationName].time == 0)
+                    {
+                        ChosenAnimation[AnimationName].time = ChosenAnimation[AnimationName].length;
+                    }
+
+                    if (Instant)
+                    {
+                        ChosenAnimation[AnimationName].time = 0;
+                    }
+
+
+                    ChosenAnimation[AnimationName].speed = -1;
+                }
+                float timeFloat = ChosenAnimation[AnimationName].time;
+
+                ChosenAnimation.Play(AnimationName);
+                ChosenAnimation[AnimationName].time = timeFloat;
+                return ChosenAnimation[AnimationName];
+            }
+        }
+
+        Debug.LogWarning("No animation found");
+        return new AnimationState();
+    }
+}
