a37ccbac489967f2d6184dfdc9386473d536b552
Allister Hamilton <allister.ham@gmail.com>
2022-06-01 13:57:40 +1200

    customers will now avoid going to a position that is already taken

--- Donut Burnout/Assets/MechanicsManager.cs
+++ Donut Burnout/Assets/MechanicsManager.cs
@@ -19,6 +19,8 @@ public class MechanicsManager : MonoBehaviour
     public class CustomerData
     {
         public Transform CustomerTransform;
+        public Transform PositionTransform;
+
         public int CustomerStatusInt;
         public float WaitTimerFloat;
         public float WaitThresholdFloat;
@@ -35,7 +37,28 @@ public class MechanicsManager : MonoBehaviour
 
     Transform ReturnRandomChild(Transform positionsTransform)
     {
-        return positionsTransform.GetChild(Random.Range(0, positionsTransform.childCount));
+        List<Transform> vacentTransformList = new List<Transform>();
+        for (int i = 0; i < positionsTransform.childCount; i++)
+        {
+            bool vacentBool = true;
+
+            for (int j = 0; j < CustomerList.Count; j++)
+            {
+                if (CustomerList[j].PositionTransform == positionsTransform.GetChild(i))
+                {
+                    vacentBool = false;
+                    break;
+                }
+            }
+
+            if (vacentBool)
+                vacentTransformList.Add(positionsTransform.GetChild(i));
+        }
+
+        if (vacentTransformList.Count > 0)
+            return vacentTransformList[Random.Range(0, vacentTransformList.Count)];
+        else
+            return positionsTransform.GetChild(Random.Range(0, positionsTransform.childCount));
 
     }
     void CreateCustomerVoid()
@@ -72,7 +95,7 @@ public class MechanicsManager : MonoBehaviour
 
         for (int i = 0; i < CustomerList.Count; i++)
         {
-            if (CustomerList[i].CustomerStatusInt == 0 || Vector3.Distance(CustomerList[i].CustomerTransform.position, CustomerList[i].CustomerTransform.GetComponent<NavMeshAgent>().destination) <= 1f)
+            if (CustomerList[i].CustomerStatusInt == 0 || Vector3.Distance(CustomerList[i].CustomerTransform.position, CustomerList[i].CustomerTransform.GetComponent<NavMeshAgent>().destination) <= (CustomerList[i].CustomerStatusInt == 3 ? 5 : 1f))
             {
                 CustomerList[i].WaitTimerFloat += Time.deltaTime;
 
@@ -84,18 +107,20 @@ public class MechanicsManager : MonoBehaviour
 
                     if (CustomerList[i].CustomerStatusInt == 1)
                     {
-                        CustomerList[i].CustomerTransform.GetComponent<NavMeshAgent>().destination = ReturnRandomChild(CounterPositionsTransform).position;
+                        CustomerList[i].PositionTransform = ReturnRandomChild(CounterPositionsTransform);
                     }
 
                     if (CustomerList[i].CustomerStatusInt == 2)
                     {
-                        CustomerList[i].CustomerTransform.GetComponent<NavMeshAgent>().destination = ReturnRandomChild(TablePositionsTransform).position;
+                        CustomerList[i].PositionTransform = ReturnRandomChild(TablePositionsTransform);
                     }
 
                     if (CustomerList[i].CustomerStatusInt == 3)
                     {
-                        CustomerList[i].CustomerTransform.GetComponent<NavMeshAgent>().destination = ReturnRandomChild(EntryPositionsTransform).position;
+                        CustomerList[i].PositionTransform = ReturnRandomChild(EntryPositionsTransform);
                     }
+
+                    CustomerList[i].CustomerTransform.GetComponent<NavMeshAgent>().destination = CustomerList[i].PositionTransform.position;
 
                     if (CustomerList[i].CustomerStatusInt == 4)
                     {
